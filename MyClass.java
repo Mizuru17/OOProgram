9c0c080181b0989e089a46d1478058c503df62e7
Shapoval <nastasyia259@gmail.com>
2024-05-03 20:41:39 +0300

    Контрольна робота - практична частина

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    private int number;
+    private String name;
+
+    public MyClass(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Number: " + number);
+        System.out.println("Name: " + name);
+    }
+}
